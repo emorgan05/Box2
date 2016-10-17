90e957868ba78f5c5fd68516ba6f529b09a18f3d
emorgan05 <emorgan05@gmail.com>
2016-10-16 20:22:23 -0400

    Update HomeController.js

--- HomeController.js
+++ HomeController.js
@@ -1,3 +1,9 @@
 function HomeController($scope) {
   $scope.totalToDos = 4;
+  
+  $scope.suggestions = [
+    {text: 'Free pizza at club meetings'},
+    {text: 'End all the emails with a Laffy Taffy joke'},
+    {text: 'Sing Living on a Prayer to start each meeting'}
+    ];
 }
